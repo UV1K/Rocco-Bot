fede09426d0f979a7d7a5b0ffb9c946515bd269c
UV1K <bouzada.rafael@gmail.com>
2026-01-20 22:00:02 +0800

    Update lib.ts
    on line 253 replaced "required" with "auto"

--- src/lib.ts
+++ src/lib.ts
@@ -250,7 +250,7 @@ export async function genMistyOutput(
         stopPlaying: stopPlayingTool,
         whatSong: whatSongTool,
       },
-      toolChoice: "required",
+      toolChoice: "auto",
     });
 
     const text = response.text;
